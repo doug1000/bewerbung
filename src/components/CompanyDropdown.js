74e31499f2c42dc8374580aa9417e0ca88fce5e3
Andreas Christ <andreaschrist@outlook.com>
2024-11-14 17:35:41 +0100

    init2

--- src/components/CompanyDropdown.js
+++ src/components/CompanyDropdown.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useSpring, animated } from '@react-spring/web'; // Importiere react-spring
 import companiesData from './companies.json'; // Importiere die JSON-Daten
 
 function CompanyDropdown() {
     const [selectedCompany, setSelectedCompany] = useState(null);
+    const [show, setShow] = useState(false);
+    const [key, setKey] = useState(0);
 
     const handleChange = (event) => {
         const company = companiesData.companies.find(c => c.title === event.target.value);
         setSelectedCompany(company);
+        setShow(!!company); // Zeigt die Details an, wenn ein Unternehmen ausgewählt ist
+        setKey(prevKey => prevKey + 1); // Aktualisiert den Key, um die Animation zu erzwingen
     };
 
+    const slideIn = useSpring({
+        transform: show ? 'translateX(0)' : 'translateX(100%)',
+        opacity: show ? 1 : 0,
+    });
+
     return (
-        <div style={{ marginTop: '40px' }}>
-            <label style={{ marginRight: '40px' }} htmlFor="company-select">Wähle ein Unternehmen:</label>
+        <div>
+            <label htmlFor="company-select">Wähle ein Unternehmen:</label>
             <select id="company-select" onChange={handleChange}>
                 <option value="">--Bitte wählen--</option>
                 {companiesData.companies.map((company) => (
@@ -23,15 +34,20 @@ function CompanyDropdown() {
             </select>
 
             {selectedCompany && (
-                <div style={{ marginTop: '30px', border:'1px solid lightgrey',padding:'15px' }}> 
+                <animated.div style={slideIn} key={key}>
                     <h2>{selectedCompany.title}</h2>
                     <h3>{selectedCompany.zeitraum}</h3>
                     <p style={{ whiteSpace: 'pre-wrap' }}>{selectedCompany.description}</p>
-                    <img src={selectedCompany.image} alt={selectedCompany.title} style={{ maxWidth: '300px', height: 'auto' }} />
-                </div>
+                    <img src={selectedCompany.image} alt={selectedCompany.title} style={{ maxWidth: '100%', height: 'auto' }} />
+                </animated.div>
             )}
         </div>
     );
 }
 
 export default CompanyDropdown;
+
+
+
+
+
